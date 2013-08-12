487e5023318bf5318ed21eb26329c216915fd370
Mariano Converti <mariano.converti@southworks.net>
2013-08-12 01:06:20 -0300

    # added nuspec and script to build and publish the nuget package

---
+++ nuget/buildandpublish.cmd
@@ -0,0 +1,15 @@
+@ECHO OFF
+
+SETLOCAL
+%~d0
+CD "%~dp0"
+
+mkdir content\net40\MediaServicesExtensions
+copy ..\src\MediaServices.Client.Extensions\MediaServicesExtensions.cs content\net40\MediaServicesExtensions
+copy ..\src\MediaServices.Client.Extensions\MediaServicesExceptionParser.cs content\net40\MediaServicesExtensions
+
+NuGet Update -self
+NuGet Pack windowsazure.mediaservices.extensions.nuspec
+NuGet Push windowsazure.mediaservices.extensions.1.0.0.nupkg
+
+PAUSE
