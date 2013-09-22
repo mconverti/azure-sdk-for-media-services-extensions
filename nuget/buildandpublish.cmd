c614abe9f96ae23d9f0257a174d893ba0d7414c5
Mariano Converti <mariano.converti@southworks.net>
2013-09-22 17:36:28 -0300

    # updated scripts to build and publish NuGet package

--- nuget/buildandpublish.cmd
+++ nuget/buildandpublish.cmd
@@ -4,11 +4,13 @@ SETLOCAL
 %~d0
 CD "%~dp0"
 
-mkdir content\net40\MediaServicesExtensions
-copy ..\src\MediaServices.Client.Extensions\*.cs content\net40\MediaServicesExtensions
+SET NuGetPath=..\src\.nuget\NuGet.exe
 
-NuGet Update -self
-NuGet Pack windowsazure.mediaservices.extensions.nuspec
-NuGet Push windowsazure.mediaservices.extensions.1.0.5.nupkg
+MKDIR windowsazure.mediaservices.extensions\content\net40\MediaServicesExtensions
+COPY ..\src\MediaServices.Client.Extensions\*.cs windowsazure.mediaservices.extensions\content\net40\MediaServicesExtensions
+
+%NuGetPath% Update -self
+%NuGetPath% Pack windowsazure.mediaservices.extensions\windowsazure.mediaservices.extensions.nuspec
+%NuGetPath% Push windowsazure.mediaservices.extensions.1.0.6.nupkg
 
 PAUSE
